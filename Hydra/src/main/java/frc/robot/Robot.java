a75ed209de46da66ebdc6204372516281158a70b
AlonsoS <ghoul6413@gmail.com>
2025-07-22 14:27:58 -0300

    main
    locomoção

--- Hydra/src/main/java/frc/robot/Robot.java
+++ Hydra/src/main/java/frc/robot/Robot.java
@@ -3,14 +3,8 @@ package frc.robot;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import com.fasterxml.jackson.databind.util.NativeImageUtil;
-import edu.wpi.first.util.sendable.SendableRegistry;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
@@ -22,7 +16,7 @@ public class Robot extends TimedRobot {
 
  // private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftDrive::set, m_rightDrive::set);
   private final Joystick m_controller = new Joystick(0);
-  private double m_speed = 0.5,magnitude,X,Y,seno;
+  private double m_speed = 0.5,magnitude,X,Y,X1,Y2,seno,magnitude2,seno2;
   private int POV;
   private double LTrigger, RTrigger, rapidao, rapidao2;
   private boolean a, b , xb;
@@ -59,12 +53,18 @@ public class Robot extends TimedRobot {
     xb = m_controller.getRawButton(3);
 
     X = m_controller.getRawAxis(0);
-    Y = m_controller.getRawAxis(1);
+    Y = -m_controller.getRawAxis(1);
+    X1 = m_controller.getRawAxis(4);
+    Y2 = m_controller.getRawAxis(5);
 
     magnitude = Math.hypot(X, Y);
     magnitude = Math.max(-1, Math.min(1, magnitude));
 
+    magnitude2 = Math.hypot(X1, Y2);
+    magnitude2 = Math.max(-1, Math.min(1, magnitude2));
+
     seno = Y / magnitude;
+    seno2 = Y2 / magnitude2;
 
     m_leftDrive.set(ControlMode.PercentOutput, rapidao);
     m_leftDrive2.set(ControlMode.PercentOutput, rapidao);
@@ -73,8 +73,13 @@ public class Robot extends TimedRobot {
 
     LTrigger = m_controller.getRawAxis(2);
     RTrigger = m_controller.getRawAxis(3);
+    
+    ifanalogico();
+    ifanalogico2();
 
-    quadrante();
+    quadrantes();
+    quadrantes2();
+   
 
     POV = m_controller.getPOV(); 
     pov();
@@ -82,8 +87,8 @@ public class Robot extends TimedRobot {
     ifLTrigger();
     ifRTrigger();
 
-  ifButton();
-  dash();
+    ifButton();
+    dash();
   
 }
 
@@ -98,8 +103,11 @@ public void dash(){
   SmartDashboard.putBoolean("BNT b", b);
   SmartDashboard.putBoolean("BNT xb", xb);
   SmartDashboard.putNumber("magnitude", magnitude);
-
-
+  SmartDashboard.putNumber("X", X);
+  SmartDashboard.putNumber("Y", Y);
+  SmartDashboard.putNumber("magnitude2", magnitude2);
+  SmartDashboard.putNumber("X1", X1);
+  SmartDashboard.putNumber("Y2", Y2);
 
 }
   public void pov(){
@@ -153,18 +161,18 @@ public void dash(){
 
   public void ifLTrigger(){
     if (LTrigger> 0.04) {
-      rapidao = LTrigger;
-      rapidao2 = LTrigger;
+      rapidao = LTrigger * m_speed;
+      rapidao2 = LTrigger * m_speed;
     } 
   }
 
   public void ifRTrigger(){
-    if (RTrigger > 0.04){
-      rapidao = -RTrigger;
-      rapidao2 = -RTrigger;
+    if (RTrigger > 0.04){ 
+      rapidao = -RTrigger * m_speed;
+      rapidao2 = -RTrigger * m_speed;
     }
   }
-   public void quadrante(){
+   public void quadrantes(){
     //quadrante 1
        if (X > 0 && Y > 0) {
         rapidao = magnitude * m_speed;
@@ -177,13 +185,51 @@ public void dash(){
       } 
       //quadrante 3
       else if (X >= 0 && Y < 0) {
-        rapidao = magnitude * m_speed;	
+        rapidao = magnitude * m_speed * -1;	
         rapidao2 = (2 * seno + 1) * magnitude * m_speed;
       } 
       //quadrante 4
        else if (X < 0 && Y < 0) {
         rapidao = (2 * seno + 1) * magnitude * m_speed;
-        rapidao2 = magnitude * m_speed;
+        rapidao2 = magnitude * m_speed * -1;
        }
       }
+
+      public void quadrantes2(){
+      //quadrante 1
+      if (X1 > 0 && Y2 > 0) {
+        rapidao = magnitude2 * m_speed;
+        rapidao2 = (2 * seno2 -1) * magnitude2 * m_speed;	
+      } 
+      //quadrante 2
+      else if (X1 < 0 && Y2 > 0) {  
+        rapidao = (2 * seno2 - 1) * magnitude2 * m_speed;
+        rapidao2 = magnitude2 * m_speed;
+      } 
+      //quadrante 3
+      else if (X1 >= 0 && Y2 < 0) {
+        rapidao = magnitude2 * m_speed * -1;	
+        rapidao2 = (2 * seno2 + 1) * magnitude2 * m_speed;
+      } 
+      //quadrante 4
+       else if (X1 < 0 && Y2 < 0) {
+        rapidao = (2 * seno2 + 1) * magnitude2 * m_speed;
+        rapidao2 = magnitude2 * m_speed * -1;
+       }
     }
+
+    public void ifanalogico(){
+      if (X > 0.04 || X < -0.04 || Y > 0.04 || Y < -0.04) {
+         X1 = 0;
+         Y2 = 0;
+         magnitude2 = 0;
+      }
+    }
+    public void ifanalogico2(){
+      if (X1 > 0.04 || X1 < -0.04 || Y2 > 0.04 || Y2 < -0.04) {
+         X = 0;
+         Y = 0;
+         magnitude = 0;
+      }
+    }
+}
